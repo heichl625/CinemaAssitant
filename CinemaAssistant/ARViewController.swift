abc880ecbd3f6b33bca2251220ac59d249d68b0f
heichl625 <cheukheilo@Cheukde-MacBook-Pro-4.local>
2020-01-17 10:51:08 +0800

    Added AR Demo, User can now add their favourite movies and display them on Favourite page, Cinema Data stored in Firebase instead of a local realm file.

---
+++ CinemaAssistant/ARViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ARViewController.swift
+//  CinemaAssistant
+//
+//  Created by Cheuk Hei Lo on 17/1/2020.
+//  Copyright © 2020 Cheuk Hei Lo. All rights reserved.
+//
+
+import UIKit
+
+class ARViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
